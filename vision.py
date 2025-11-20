b79252c2eaea7fc796494b8f7ca625328a846271
rishika2024 <berarishika@gmail.com>
2025-11-19 18:53:53 -0600

    can track only 1 object so far

--- vision.py
+++ vision.py
@@ -30,6 +30,9 @@ class Vision:
         self.high_S_name = 'High S'
         self.high_V_name = 'High V'
 
+        self.tracker = Sort(max_age=15, min_hits=2, iou_threshold=0.0)
+        self.detections = []
+
         # create trackbar (blur+update combined)
         
         cv2.createTrackbar(self.low_H_name, self.window_name, self.low_H, self.max_value_H, self.on_low_H_thresh_trackbar)
@@ -71,22 +74,41 @@ class Vision:
 
     def add_contour(self,mask=None):
         # Morphological closing (fills small gaps inside the pen mask)
-        kernel = np.ones((7,7), np.uint8)
+        kernel = np.ones((3,3), np.uint8)
         mask_closed = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+        #cv2.imshow("Closed Mask", mask_closed)
         contours, hierarchy = cv2.findContours(mask_closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        return contours, hierarchy
         
-    def draw_bounding_box(self, contours):
+    def draw_bounding_box(self, contours,frame):
         if contours is None or len(contours) == 0:
-            return None  # no contours found        
+            return np.empty((0,5))  # no contours found        
         # Use largest contour
         largest_contour = max(contours, key=cv2.contourArea)
 
         # fitRectangle needs at least 4 points
         if len(largest_contour) < 4:
-            return None
+            return np.empty((0, 5))
 
-        x, y, w, h = cv2.fitboundingRect(largest_contour)        
-        return x, y, x+w, y+h
+        x, y, w, h = cv2.boundingRect(largest_contour) 
+        if w == 0 or h == 0:
+            return np.empty((0, 5))
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), -1)
+        return np.array([[x, y, x + w, y + h, 1.0]], dtype=float)
+    
+    def track_results(self, tracked, frame):
+        for x1, y1, x2, y2, track_id in tracked:
+            if np.isnan(x1) or np.isnan(y1) or np.isnan(x2) or np.isnan(y2):
+                continue
+            x1, y1, x2, y2 = map(int, [x1, y1, x2, y2])
+            cx = (x1 + x2) // 2
+            cy = (y1 + y2) // 2
+        
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.circle(frame, (cx, cy), 4, (0,255,0), -1)
+            cv2.putText(frame, f"ID {int(track_id)}", (x1, y1-10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
+
     
     def run(self):
         if not self.cap.isOpened():
@@ -102,12 +124,24 @@ class Vision:
             frame_threshold = cv2.inRange(frame_HSV, (self.low_H, self.low_S, self.low_V), (self.high_H, self.high_S, self.high_V))
             only_rgb = cv2.bitwise_and(frame, frame, mask=frame_threshold)
             
+            
 
             self.current_frame = frame
+
+            contour, heirarchy = self.add_contour(frame_threshold)
+            detections = self.draw_bounding_box(contours=contour, frame=only_rgb)
+            
+            tracked = self.tracker.update(detections)
+            print(f"Detections: {detections}, Tracked: {tracked}")
+            
+            self.track_results(tracked, only_rgb)
+
 
             cv2.imshow(self.window_name, frame)
             cv2.imshow(self.window_mask_name, frame_threshold)
             cv2.imshow(self.window_only_rgb, only_rgb)
+
+            
 
             # --- allow quitting with Q ---
             if cv2.waitKey(1) & 0xFF == ord('q'):
