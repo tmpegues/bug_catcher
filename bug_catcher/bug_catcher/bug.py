b58024a15b6ad90f41a128338b6667078ffa6af7
Miguel Pegues <MiguelP@u.northwestern.edu>
2025-11-19 19:25:37 -0600

    Continuing Bug class

--- bug_catcher/bug_catcher/bug.py
+++ bug_catcher/bug_catcher/bug.py
@@ -2,6 +2,7 @@
 
 from enum import auto, Enum
 
+import numpy as np
 from geometry_msgs.msg import PoseStamped, TwistStamped
 
 
@@ -19,18 +20,22 @@ class Color(Enum):
 class Bug:
     """A class to represent a single HexBug."""
 
-    def __init__(self, pose: PoseStamped, color: Color):
+    def __init__(self, id: int, pose: PoseStamped, color: Color):
         """
         Initialize the Bug.
 
         Args:
         ----
+        id (int): a unique id for each bug
         pose (PoseStamped): the pose of the bug at the time of the camera frame
         color (Color): the color of the bug
 
         """
+        self.id = id
         self.pose = pose
         self.color = color
+
+        self.vel = TwistStamped()
 
     def update(self, new_pose: PoseStamped):
         """
@@ -44,6 +49,35 @@ class Bug:
         self._calc_vel(new_pose, self.pose)
         self.pose = new_pose
 
+    # This conversion needs to be done because the twist.angular is euler and pose.orient is quat
+    def _euler_from_quaternion(self, quat):
+        """
+        Calculate Euler angles from a quaternion.
+
+        Args:
+        ----
+        quat (Quaternion): a quaternion to convert to Euler angles
+
+        Returns
+        -------
+        rx, py, yz (floats): roll, pitch, yaw angles corresponding to quat
+
+        """
+        t0 = +2.0 * (quat.w * quat.x + quat.y * quat.z)
+        t1 = +1.0 - 2.0 * (quat.x * quat.x + quat.y * quat.y)
+        rx = np.arctan2(t0, t1)
+
+        t2 = +2.0 * (quat.w * quat.y - quat.z * quat.x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        py = np.arcsin(t2)
+
+        t3 = +2.0 * (quat.w * quat.z + quat.x * quat.y)
+        t4 = +1.0 - 2.0 * (quat.y * quat.y + quat.z * quat.z)
+        yz = np.arctan2(t3, t4)
+
+        return rx, py, yz
+
     def _calc_vel(self, new_pose: PoseStamped, old_pose: PoseStamped):
         """
         Calculate the velocity of the bug based on the last two camera frames.
@@ -56,5 +90,11 @@ class Bug:
         """
         vel = TwistStamped()
         x = new_pose.pose.position.x - old_pose.pose.position.x
-        vel.twist.linear.x = x
-        self.vel = vel
+        y = new_pose.pose.position.y - old_pose.pose.position.y
+        z = new_pose.pose.position.z - old_pose.pose.position.z
+        vel.twist.linear.x, vel.twist.linear.y, vel.twist.linear.z = x, y, z
+
+        angles_old = [self._euler_from_quaternion(old_pose.pose.orientation)]
+        angles_new = [self._euler_from_quaternion(new_pose.pose.orientation)]
+
+        self.angle_diff = angles_new - angles_old
