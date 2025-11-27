d7c68510ad50dc0ccd8a16d5bba70f919a6241c2
Nolan Knight <nolanknight2026@u.northwestern.edu>
2025-11-26 18:00:58 -0600

    Fixing Pull request issue.

--- bug_catcher/setup.py
+++ bug_catcher/setup.py
@@ -48,7 +48,7 @@ setup(
         'console_scripts': [
             'catcher_node = bug_catcher.catcher_node:main',
             'pick_node = bug_catcher.pick_node:main',
-            'calibration_node = bug_catcher.Calibration_TargetPublisher:main'
+            'calibration_node = bug_catcher.Calibration_TargetPublisher:main',
             'camera = bug_catcher.camera:main',
         ],
     },
